e87b1db6d864ccdd6aa339b51721a853e6cf6b63
aleksandr_gvozdev <wayk5i@yandex.ru>
2025-11-05 20:19:52 +0300

    добавил стили шапки для мобилки

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,7 @@
-/* в этот файл добавляет скрипты*/
+const nav = document.querySelector('.main-nav');
+const openButton = document.querySelector('.user-nav__button--toggle');
+
+openButton.onclick = function () {
+  nav.classList.toggle('main-nav--is-close');
+};
+
